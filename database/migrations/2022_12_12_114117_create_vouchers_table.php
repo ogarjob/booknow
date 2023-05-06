2e17eb23fdef411d46d161bff30f3db73d62c698
ogarjob <ogarjobbassey@gmail.com>
2023-05-06 23:55:10 +0100

    Create `vouchers` migration

---
+++ database/migrations/2022_12_12_114117_create_vouchers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_id')->nullable()->constrained();
+            $table->string('login', 45);
+            $table->string('password', 45);
+            $table->tinyInteger('days');
+            $table->authors();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
