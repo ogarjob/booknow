f49da6300dc18879ce1731543b8f083f11cff9f3
ogarjob <ogarjobbassey@gmail.com>
2023-05-06 09:38:06 +0100

    Update `users` migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,30 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name', 45);
+            $table->string('last_name', 45)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('phone', 45)->nullable()->unique();
+            $table->enum('gender', ['M', 'F'])->default('M')->index();
+            $table->date('date_of_birth')->nullable();
+            $table->text('address')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('id_card')->nullable();
+            $table->enum('type', ['admin', 'agent', 'customer', 'vendor'])->nullable()->default('customer');
+            $table->foreignId('role_id')->nullable()->constrained();
+            $table->boolean('notifiable')->default(false);
+            $table->string('password')->nullable();
+            $table->integer('login_count')->default(0);
+            $table->timestamp('last_login')->nullable();
+            $table->dateTime('banned_until')->nullable();
+            $table->json('meta')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->authors();
+            $table->softDeletes();
+
+            $table->fullText(['last_name', 'first_name', 'phone', 'email']);
         });
     }
 
@@ -29,4 +47,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
