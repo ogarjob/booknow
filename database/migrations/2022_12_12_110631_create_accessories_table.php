42557d192e8e63de633e8b5a5034e0636fb0b942
ogarjob <ogarjobbassey@gmail.com>
2023-05-06 09:44:40 +0100

    Create `accessories` migration

---
+++ database/migrations/2022_12_12_110631_create_accessories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accessories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->integer('stock')->nullable();
+            $table->authors();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accessories');
+    }
+};
