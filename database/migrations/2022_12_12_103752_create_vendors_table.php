d4f9f837b76efad3b2ba419cf4897c6e2bc79e2b
ogarjob <ogarjobbassey@gmail.com>
2023-05-06 09:41:09 +0100

    Create `vendors` migration

---
+++ database/migrations/2022_12_12_103752_create_vendors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name', 45);
+            $table->string('logo')->nullable();
+            $table->float('markup')->default(1.50);
+            $table->authors();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
