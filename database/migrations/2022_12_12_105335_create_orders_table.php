85cefcd7757365891da01e99c0b1ee54ab434aea
ogarjob <ogarjobbassey@gmail.com>
2023-05-06 09:44:12 +0100

    Create `orders` migration

---
+++ database/migrations/2022_12_12_105335_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_id')->nullable()->constrained();
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->float('discount')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamp('delivered_at')->nullable();
+            $table->authors();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
