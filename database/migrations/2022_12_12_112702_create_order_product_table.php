8e44065198e2e3bf6acbbceba28871e4c1e6308a
ogarjob <ogarjobbassey@gmail.com>
2023-05-06 09:45:55 +0100

    Create `order_product` migration

---
+++ database/migrations/2022_12_12_112702_create_order_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->json('meta')->nullable();
+            $table->authors();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
