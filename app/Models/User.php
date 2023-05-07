8c376d57e58996157f6a7fd1e2d2e2d302aec7c0
ogarjob <ogarjobbassey@gmail.com>
2023-05-07 23:27:54 +0100

    Update `User` Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,36 +2,43 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Enums\Gender;
+use App\Models\Traits\HasMetaAttribute;
+use App\Models\Traits\InteractsWithUserAttributes;
+use App\Models\Traits\ObservesWrites;
+use App\Models\Traits\UserScope;
+use App\Observers\Observable;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Traits\Tappable;
+use Lab404\Impersonate\Models\Impersonate;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, Observable, InteractsWithUserAttributes,
+        ObservesWrites, HasMetaAttribute, UserScope, Impersonate, Tappable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that aren't mass assignable.
      *
-     * @var array<int, string>
+     * @var array<string>|bool
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = ['login_count', 'email_verified_at'];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token', 'meta'];
 
     /**
      * The attributes that should be cast.
@@ -40,5 +47,55 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'banned_until'      => 'datetime',
+        'last_login'        => 'datetime',
+        'gender'            => Gender::class
     ];
+
+    /**
+     * The event map for the model.
+     *
+     * Allows for object-based events for native Eloquent events.
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'created' => Registered::class
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     */
+    protected $appends = ['name'];
+
+    /**
+     * Determine if the user can impersonate another user.
+     */
+    public function canImpersonate(): bool
+    {
+        return $this->isAdmin();
+    }
+
+    /**
+     * Determine if the user can be impersonated by another user.
+     */
+    public function canBeImpersonated(): bool
+    {
+        return ! $this->isSuperAdmin() && user()->canImpersonate();
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class)->withoutEagerLoad(['user']);
+    }
+
+    public function vendor(): HasOne
+    {
+        return $this->hasOne(Vendor::class);
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
